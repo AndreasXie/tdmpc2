73d19556bb92500fc95c7a465cdeb8bd327a1e66
dawn1206 <1206470742@qq.com>
2024-12-02 01:57:59 +0000

    fix no mpc, use one hot as output and argmax in env wrapper

--- tdmpc2/tdmpc2.py
+++ tdmpc2/tdmpc2.py
@@ -108,7 +108,7 @@ class TDMPC2(torch.nn.Module):
 			action = self.plan(obs, t0=t0, eval_mode=eval_mode, task=task)
 		else:
 			z = self.model.encode(obs, task)
-			action = self.model.pi(z, task)[0]
+			action = self.model.pi(z, task)[1]
 			if self.cfg.action == 'discrete':
 				action = action.squeeze(0) # TODO: this is a bit hacky
 		return action.cpu()
