29ed82c2d518686c53844d5cae425495feedd94c
AndreasXie <yux076@ucsd.edu>
2024-12-14 16:31:15 -0800

    test totally use cuda for storage

--- tdmpc2/common/buffer.py
+++ tdmpc2/common/buffer.py
@@ -58,7 +58,8 @@ class Buffer():
 		total_bytes = bytes_per_step*self._capacity
 		print(f'Storage required: {total_bytes/1e9:.2f} GB')
 		# Heuristic: decide whether to use CUDA or CPU memory
-		storage_device = 'cuda:0' if 2.5*total_bytes < mem_free else 'cpu'
+		# storage_device = 'cuda:0' if 2.5*total_bytes < mem_free else 'cpu'
+		storage_device = 'cuda:0'
 		print(f'Using {storage_device} memory for storage.')
 		self._storage_device = torch.device(storage_device)
 		# return self._reserve_buffer(
